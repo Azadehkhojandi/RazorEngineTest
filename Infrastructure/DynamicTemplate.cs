e1958c0cf21de33a6888207034bae216a3996669
Azadeh Khojandi <azadehkhojandi@gmail.com>
2012-05-20 21:02:50 +1000

    #updated#

---
+++ Infrastructure/DynamicTemplate.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RazorEngine.Templating;
+using System.Reflection;
+using System.IO;
+
+namespace Infrastructure
+{
+    public static class DynamicTemplate
+    {
+        public static string GiveCshtml<T>(T model,string templateName="GeneralInformation")
+        {
+
+            using (var service = new TemplateService())
+            {
+                var layoutTemplate = new StreamReader(Assembly.GetExecutingAssembly().GetManifestResourceStream("Infrastructure.Templates._Layout.cshtml")).ReadToEnd();
+                var page = new StreamReader(Assembly.GetExecutingAssembly().GetManifestResourceStream(string.Format("Infrastructure.Templates.{0}.cshtml", templateName))).ReadToEnd();
+
+                service.Compile(layoutTemplate, typeof(T), "_Layout");
+
+                return service.Parse(page, model);
+                
+            }
+        }
+    }
+}
---
+++ RazorEngineTest/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RazorEngineTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Default1/
+
+        public ActionResult Index()
+        {
+
+            var result = Infrastructure.DynamicTemplate.GiveCshtml(new
+            {
+                Title = "Top 10 books",
+                Books = new List<string> {
+                    "Harry poter1",
+                    "Harry poter2",
+                    "Harry poter3",
+                    "The hunger game1" ,
+                    "Harry poter1", 
+                    "The Hobbit", 
+                    "Eat Pray Love", 
+                    "A Tale of Two Cities",
+                    "The Lion, the Witch and the Wardrobe",
+                    "Think and Grow Rich" }
+            });
+
+            return Content(result);
+        }
+
+    }
+}
